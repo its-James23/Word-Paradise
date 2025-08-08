cd0db2bfe1c753d72fa2567df40e7701f530380a
Samuel Nwaokocha <samuel.nwaokocha@techskool.org>
2025-08-07 17:32:00 -0700

    Created code

---
+++ JS/fire.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyA-M34pq8rztRf_s5M9TnVo9bRsLn3vVgo",
+    authDomain: "paradiso-57343.firebaseapp.com",
+    projectId: "paradiso-57343",
+    storageBucket: "paradiso-57343.firebasestorage.app",
+    messagingSenderId: "335140844141",
+    appId: "1:335140844141:web:a2a35e9732604665a18564"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
