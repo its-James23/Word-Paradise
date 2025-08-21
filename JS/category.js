49be397877ffef7a00ed0e9254d6d306d315d956
Samuel Nwaokocha <samuel.nwaokocha@techskool.org>
2025-08-21 00:03:58 -0700

    Few Changes
    -Did a little stuff

--- JS/category.js
+++ JS/category.js
@@ -1,4 +1,4 @@
-import {setInHistory} from './utility.js'
+import { setInHistory, setInFavorites } from "./utility.js";
 const parameter = new URLSearchParams(window.location.search);
 const categoryName = parameter.get("name") || "fiction";
 
@@ -25,18 +25,28 @@ function loadBooks(book) {
       }
       let downloadUrl =
         page.formats["text/html; charset=utf-8"] ||
-        page.formats["application/epub+zip"] ||
-        page.formats["text/plain; charset=utf-8"];
+        page.formats["text/plain; charset=utf-8"] ||
+        page.formats["application/epub+zip"];
       let bookDiv = document.createElement("div");
       let url = `https://www.gutenberg.org/cache/epub/${page.id}/pg${page.id}.cover.medium.jpg`;
-      bookDiv.className = "p-3 book";
+      bookDiv.className = "p-3 book position-relative";
       bookDiv.innerHTML = `
         <img src="${url}" onerror="this.onerror=null; this.src='https://picsum.photos/150/200';"  alt="${rest}"/>
         <p class="small text-center">${rest}</p>
-        <a href="${downloadUrl}" class="btn btn-primary germinate" download>Download</a>
+        <div class="d-inline-flex justify-content-between align-items-center w-100 bottom-0">
+          <a href="${downloadUrl}" class="btn btn-primary germinate" download>Download</a>
+          <button class="btn btn-outline-danger d-flex align-items-center gap-1 faves" data-id="${page.id}"> <i class="bi bi-heart"></i><span>Favorite</span></button>
+            <span class="position-absolute visually-hidden top-0 start-100 translate-middle love badge rounded-pill bg-danger">
+                <i class="bi bi-heart-fill"></i>
+                <span class="visually-hidden">Faves</span>
+            </span>
+        </div>
     `;
       let btn = bookDiv.querySelector(".germinate");
-      btn.addEventListener("click", () => setInHistory(page))
+      btn.addEventListener("click", () => setInHistory(page));
+
+      let favebtn = bookDiv.querySelector(".faves")
+      favebtn.addEventListener("click", () => setInFavorites(page, favebtn));
       container.appendChild(bookDiv);
     });
   } catch (err) {
--- JS/utility.js
+++ JS/utility.js
@@ -1,11 +1,29 @@
-export function setInHistory(bookData){
-    let history = JSON.parse(localStorage.getItem()) || []
-    let dataStore = {
-        title: bookData.title,
-        author: bookData.author,
-        date: new Date.toISOString()
+export function setInHistory(bookData) {
+  let history = JSON.parse(localStorage.getItem("history")) || [];
+  let dataStore = {
+    title: bookData.title,
+    author: bookData.authors?.[0]?.name || "Unknown Author",
+    date: new Date().toISOString(),
+  };
+  console.log("it worked");
+  history.unshift(dataStore);
+  localStorage.setItem("history", JSON.stringify(history));
+}
+export function setInFavorites(bookInfo, button1){
+    let favorites = JSON.parse(localStorage.getItem("favorites")) || []
+    let faveStore = {
+        id: bookInfo.id,
+        title: bookInfo.title,
+        author: bookInfo.authors?.[0]?.name ||"Unknown Author"
     }
-    console.log('it worked')
-    history.unshift(dataStore)
-    localStorage.setItem("history", history)
+    if(!favorites.some(best => best.id === bookInfo.id)){
+            console.log("saved to favorites");
+            favorites.unshift(faveStore);
+            localStorage.setItem("favorites", JSON.stringify(favorites)); 
+            let span = button1.parentElement.querySelector(".love");
+            span.classList.remove("visually-hidden")
+    } else{
+        alert("Already in favorites");
+        console.log("Its already there, ya big lug! 😁")
+    }
 }
